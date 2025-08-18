d721dec8af88deb607b10a3ecb2ad6d3ff09200e
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-08-18 17:45:31 +0300

    fix: add varial for button component and one more color variable

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,14 +1,24 @@
 import clsx from 'clsx';
 import s from './Button.module.css';
 
+type ButtonVariant = 'primary' | 'secondary';
+
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   children: string;
   className: string;
+  variant?: ButtonVariant;
 }
 
-const Button = ({ children, className, ...rest }: ButtonProps) => {
+const Button = ({ children, className, variant = 'primary', ...rest }: ButtonProps) => {
   return (
-    <button type='button' {...rest} className={clsx(className, s.button)}>
+    <button
+      type='button'
+      {...rest}
+      className={clsx(className, s.button, {
+        [s.buttonPrimary]: variant === 'primary',
+        [s.buttonSecondary]: variant === 'secondary',
+      })}
+    >
       {children}
     </button>
   );
