be598d008a4f759db798b6c0c563fc8046ed56a5
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-08-20 10:55:54 +0300

    feat: add suspense

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import CampersPage from './pages/CampersPage/CampersPage';
 import CamperPage from './pages/CamperPage/CamperPage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import Header from './components/Header/Header';
+import { Suspense } from 'react';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/campers' element={<CampersPage />} />
-          <Route path='/campers/:id' element={<CamperPage />} />
-          <Route path='*' element={<NotFoundPage />} />
-        </Routes>
+        <Suspense fallback={<h2>Loading...</h2>}>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/campers' element={<CampersPage />} />
+            <Route path='/campers/:id' element={<CamperPage />} />
+            <Route path='*' element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
       </main>
     </>
   );
