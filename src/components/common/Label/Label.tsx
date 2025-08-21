587f66c12be6214a8e6262910a0f9f2ddd43818a
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-08-21 13:15:54 +0300

    feat: add feat list on the camper page

--- src/components/common/Label/Label.tsx
+++ src/components/common/Label/Label.tsx
@@ -1,14 +1,16 @@
+import clsx from 'clsx';
 import s from './Label.module.css';
 
 export interface LabelProps {
   icon: string;
   text: string | boolean;
+  className: string;
 }
 
-export default function Label({ icon, text }: LabelProps) {
+export default function Label({ icon, text, className }: LabelProps) {
   return (
     <div className={s.labelContainer}>
-      <svg width={20} height={20} className={s.labelIcon}>
+      <svg width={20} height={20} className={clsx(s.labelIcon, className)}>
         <use href={icon}></use>
       </svg>
       <p className={s.labelText}>{text}</p>
